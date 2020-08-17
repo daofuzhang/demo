06bf203158637014bd3840889b748810c63eaa08
daofuzhang <daofu_zhang@163.com>
2020-08-17 17:23:21 +0800

    2020

--- demo-want/demo-ws/src/main/java/com/demo/ws/client/TestWs.java
+++ demo-want/demo-ws/src/main/java/com/demo/ws/client/TestWs.java
@@ -2,8 +2,6 @@ package com.demo.ws.client;
 
 import java.net.MalformedURLException;
 
-import com.demo.ws.client.DTZRFCHR049REQ.IOTPA0002;
-import com.demo.ws.client.DTZRFCHR049REQ.IOTPA0021;
 
 public class TestWs {
 
@@ -11,12 +9,12 @@ public class TestWs {
 		SIOZRFCHR049 ws =  new SIOZRFCHR049Service().getHTTP_Port();
 		DTZRFCHR049REQ req = new DTZRFCHR049REQ();
 		
-		IOTPA0002  ito002 =new IOTPA0002();
+		com.demo.ws.client.DTZRFCHR049REQ.IOTPA0002  ito002 =new com.demo.ws.client.DTZRFCHR049REQ.IOTPA0002();
 		ito002.setPERNR("00220447");
 		req.getIOTPA0002().add(ito002);
 		
 		
-		IOTPA0021 ito021 =new IOTPA0021();
+		com.demo.ws.client.DTZRFCHR049REQ.IOTPA0021 ito021 =new com.demo.ws.client.DTZRFCHR049REQ.IOTPA0021();
 		ito021.setPERNR("00220439");
 		ito021.setZZPERNR("00767832");
 		req.getIOTPA0021().add(ito021);
